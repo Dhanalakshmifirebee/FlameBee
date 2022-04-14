caf682dd10b117e8b3f8c2def8a6b6c307635778
Faizul <faizul.n@firebeetechnoservices.com>
2022-04-14 18:54:40 +0530

    FlameBee

---
+++ model/restaraunt_enroll_schema.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose')
+
+const imageSchema = mongoose.Schema({
+    image:String
+})
+
+
+const restrauntSchema = mongoose.Schema({
+    email:String,
+    restaurantName: String,
+    restaurantImage:String,
+    menuImage:String,
+    contact:String,
+    typeOfFood:String,
+    cuisine:String,
+    address: String,
+    userId:String,
+    // restaurantAdminName:String,
+    // restaurantAdminPassword:String,
+    // restaurantContact:Number,
+    // categories:[String],
+    // review:String,address
+    // rating:{
+    //     ratingName:String,
+    //     ratingNumber:String
+    // },
+    
+    // city:String,
+    // country:String,
+    // location:{
+    //     restaurantLatitude:Number,
+    //     restaurantLongitude:Number
+    // },
+    // information:String,
+    deleteFlag: {
+        type: String,
+        default: 'false'
+    }
+})
+
+
+const restaurantImageSchema=mongoose.Schema({
+    restaurantImage:String,
+    restaurantId:String,
+    deleteFlag:{
+        type:Boolean,
+        default:false
+    }
+})
+
+const avaliableFoodSchema = mongoose.Schema({
+    foodName: String,
+    foodImage: String,
+    price: Number,
+    offer: String,
+    typeOfFood: String,
+    category:String,
+    cuisine:String,
+    rating:String,
+    amount: {
+        type: Number,
+        default: 1
+    },
+    deleteFlag: {
+        type: String,
+        default: 'false'
+    }
+})
+
+const image = mongoose.model('imageSchema',imageSchema)
+const addAvaliableFood = mongoose.model('foodSchema', avaliableFoodSchema)
+const addRestrauntByOwner = mongoose.model('restrauntSchema', restrauntSchema)
+// const restaurantImage = mongoose.model('restaurantImageSchema', restaurantImageSchema)
+
+
+module.exports = { 
+    addRestrauntByOwner, 
+    addAvaliableFood,
+    image
+    // restaurantImage 
+}
---
+++ routes/restaraunt_routes.js
@@ -0,0 +1,25 @@
+const router=require('express').Router()
+const restarauntControl=require('../controller/restaraunt_controller')
+const multer=require('../middleware/multer')
+
+// router.get('/getRestarauntList',restarauntControl.listRestarauntName)
+router.post('/image',multer.upload.single('image'),restarauntControl.image)
+router.post('/createRestaurant',restarauntControl.createRestarauntBySuper)
+router.get('/getSpecificRestaurantCreatedByOwner',restarauntControl.getRestarauntByOwner)
+router.get('/textSearchRestaurantName/:restaurantId',restarauntControl.textSearchRestaurantName)
+router.get('/getAllRestaurant',restarauntControl.getAllRestaraunt)
+// router.get('/getAllRestarauntCreatedByOwner',restarauntControl.getAllRestarauntCreatedByOwner)
+router.get('/getByRestaurantId/:id',restarauntControl.getByRestarauntId)
+router.put('/updateRestaurant/:id',restarauntControl.updateRestarauntByOwner)
+router.delete('/deleteRestaurant/:id',restarauntControl.deleteRestarauntByOwner)
+
+
+//available food 
+router.post('/avaliablefood',multer.upload.single('foodImage'),restarauntControl.avaliableItems)
+router.get('/getFoodByOwner/:restaurantId',restarauntControl.getFoodItemsByOwner)
+router.get('/getByFoodId/:id',restarauntControl.getByFoodId)
+router.get('/getAllFood',restarauntControl.getAllFood)
+router.put('/updateFood/:id',multer.upload.single('foodImage'),restarauntControl.updateFood)
+router.delete('/deleteFoodItems/:id',restarauntControl.deleteFoodItems)
+
+module.exports=router
